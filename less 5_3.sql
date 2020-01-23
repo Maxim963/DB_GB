7ed17c89be8d48328b82160e9960d00b93b15c06
Maxim963 <m.m.n.1984@yandex.ru>
2020-01-23 21:03:53 +0300

    В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры: 0, если товар закончился и выше нуля, если на складе имеются запасы. Необходимо отсортировать записи таким образом, чтобы они выводились в порядке увеличения значения value. Однако, нулевые запасы должны выводиться в конце, после всех записей.

---
+++ less 5_3.sql
@@ -0,0 +1,24 @@
+use shop;
+DROP TABLE IF EXISTS storehouses_products;
+CREATE TABLE storehouses_products (
+  id SERIAL PRIMARY KEY,
+    storehouse_id int unsigned,
+    product_id int unsigned,
+    value int unsigned comment 'Запас товарной позиции на складе',
+    created_at DATETIME default current_timestamp COMMENT 'Дата регистрации',
+    updated_at DATETIME default current_timestamp ON UPDATE current_timestamp COMMENT 'Дата последнего обновления записи'
+) COMMENT = 'Склады и продукты';
+
+INSERT INTO storehouses_products (storehouse_id, product_id, value) VALUES
+  (1, 1, 0),
+    (1, 2, 5),
+    (1, 3, 0),
+    (2, 4, 23),
+    (2, 5, 51),
+    (1, 6, 0),
+    (1, 9, 0),
+    (1, 7, 4);
+    
+SELECT * FROM storehouses_products
+  ORDER BY CASE WHEN value = 0 THEN 2147483647 ELSE value end;
+  
